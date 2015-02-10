800d36b13ba07271c8858f6b14ae17f508de6413
高鹏 <rockts@free-e.net>
2015-02-10 21:12:45 +0800

    新增 计算表单总金额代码

--- processorder.php
+++ processorder.php
@@ -2,8 +2,9 @@
 //create short variable names
 $tireqty=$ _POST [ 'tireqty'];
 $oilqty=$ _POST [ 'oilqty'];
-$sparkqty=$ _POST[ 'sparkqty'];
+$sparkqty=$_POST[ 'sparkqty'];
 ?>
+
 <html>
 
 <head>
@@ -13,13 +14,31 @@ $sparkqty=$ _POST[ 'sparkqty'];
 <body>
     <h1>Bob 's Auto Parts</h1>
     <h2>Order Results</h2>
-         <?php
-          echo "<p>Order processed at " .date( 'H:i, jS F Y'). "</p>";
-          echo '<p>Your order is as follows:</p>';
-          echo $tireqty. 'tires <br/>';
-          echo $oilqty. 'bottles if oil </br>';
-          echo $sparkqty.'spark plugs </br>';
-          ?>
+    <?php
+        echo "<p>Order processed at " .date( 'H:i, jS F Y'). "</p>";
+
+        $totalqty=0 ;
+        $totalqty=$tireqty + $oilqty + $sparkqty ;
+
+        echo "Items ordered: " .$totalqty. "<br />";
+        $totalamount=0 .00;
+
+        define( 'TIREPRICE' , 100);
+        define( 'OILPRICE' , 10);
+        define( 'SPARKPRICE', 4);
+
+        $totalamount = $tireqty * TIREPRICE
+                     + $oilqty * OILPRICE
+                     + $sparkqty * SPARKPRICE;
+
+        echo "Subtotal: $".number_format($totala,ount,2) . "<br />";
+
+        $taxrate = 0.10; //local sales tax is 10%
+        $totalamount = $totalamount * (1 + taxrate);
+        echo "Total including tax: $" .number_format($totalamount,2). <br />";
+
+    ?>
 </body>
 
 </html>
+
