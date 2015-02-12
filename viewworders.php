21965e1fb6c5c616a1d5a84573e8b8ba3cf3d13d
高鹏 <rockts@free-e.net>
2015-02-12 11:37:26 +0800

    订单查询页面

--- viewworders.php
+++ viewworders.php
@@ -13,17 +13,14 @@ $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
            @$fp = fopen("$DOCUMENT_ROOT/orders/orders.txt", 'rb');
             flock($fp, LOCK_SH); // lock file for reading
             // read from the file
-
-
            if (!$fp) {
                echo "<p><strong>No orders pending.
                      please try again later.</strong></p>";
                exit;
            }
-
           while (!feof($fp)) {
-              $order= fgets($fp, 999);
-              echo $order."<br />";
+              $orders= fgets($fp, 999);
+              echo $orders."<br />";
           }
             flock($fp, LOCK_UN); // release read lock
             fclose($fp);
